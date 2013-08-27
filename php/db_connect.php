30cdfb6c2b895ec2afb2e00349784da2fb02ad86
Stephen Quenzer <swquenzer@gmail.com>
2013-08-27 10:40:25 -0400

    update database password

--- php/db_connect.php
+++ php/db_connect.php
@@ -1,7 +1,7 @@
 <?php
 $dbHost = "localhost";
 $dbUser = "root";
-$dbPass = "p@ssw0rd";
+$dbPass = "kingku610";
 #Connect to database
 $dbHandle = mysqli_connect($dbHost,$dbUser,$dbPass)
 			or die("Error connecting to SQL Server on $dbHost");
