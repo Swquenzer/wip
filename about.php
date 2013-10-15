903ab2ed7d406affd8d8836b4bd088c32251be0b
Stephen Quenzer <swquenzer@gmail.com>
2013-10-14 20:07:42 -0400

    Create about.php -Update global.css for about page

---
+++ about.php
@@ -0,0 +1,28 @@
+<?php include 'php/header.php'; ?>
+<!-- Put any page-specific head elements here -->
+</head>
+<?php include 'php/nav.php'; ?>
+		<div id="pageContent">
+			<?php include "php/col1.php"; ?>
+			<div id="colMain"> <!-- ### MAIN CONTENT ### -->
+				<h1>What are we?</h1>
+				<div id="about">
+								<h2>Sit down.</h2> 
+								<h3>Grab a cold drink, focus your mind, and create something cool.</h3>
+								<p>
+									The Imagination Den is made for you to show off your mad skills, share them with others, and boost your emerging talents with the help of your new network of like-minded creatavists. This is your new portfolio-building paradise, where you can interact with thousands of other artists, authors, poets, programmers, and all who are entranced by the possibilities of our imagination.
+								</p>
+								<ul>
+									<li><h4>Create:</h4>
+									<p>Build your own personal portfolio through the creation of individual projects, which might range from a day-long watercolor study of the human form to a 6-month collaborative venture to build a 3D representation of New York City.</p>
+									</li>
+									<li><h4>Share:</h4>
+									<p>As you begin your new project, publish the work you create to your friends or the public for critique and inspiration. </p>
+									</li>
+									<li><h4>Connect:</h4>
+									<p>As you begin the construction of your own portfolio, connect with other like-minded folk from around the world and create your own personal or professional network.</p>
+									</li>
+								</ul>
+								</div><!--End Admin Area Div-->
+			</div> <!--End col2-->
+			<?php include "php/col3_footer.php"; ?>
--- php/nav.php
+++ php/nav.php
@@ -7,7 +7,7 @@ echo '
 					<ul id="navList">
 						<li><a href="/wip/index.php">Home</a></li>
 						<li><a href="#">Projects</a></li>
-						<li><a href="#">About</a></li>
+						<li><a href="about.php">About</a></li>
 						<li><a href="#">Contact</a></li>
 						<li id="loginLink"><a id="loginLinkA" href="/wip/login.php">Default</a></li>
 					</ul>
