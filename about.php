0d6baac51b7ad6c83d3a36a3b0c9030fcd9cd1e0
Stephen Quenzer <swquenzer@gmail.com>
2013-10-15 11:26:27 -0400

    Update styles for about.php

--- about.php
+++ about.php
@@ -9,12 +9,14 @@
 				<div id="about">
 								<h2>Sit down.</h2> 
 								<h3>Grab a cold drink, focus your mind, and create something cool.</h3>
+								<hr/>
+								<span id="test">
 								<p>
 									The Imagination Den is made for you to show off your mad skills, share them with others, and boost your emerging talents with the help of your new network of like-minded creatavists. This is your new portfolio-building paradise, where you can interact with thousands of other artists, authors, poets, programmers, and all who are entranced by the possibilities of our imagination.
 								</p>
 								<ul>
 									<li><h4>Create:</h4>
-									<p>Build your own personal portfolio through the creation of individual projects, which might range from a day-long watercolor study of the human form to a 6-month collaborative venture to build a 3D representation of New York City.</p>
+									<p>Build your own personal portfolio through the creation of individual projects, which might range from a day-long watercolor study of the human form to a 6-month collaborative venture to build a 3D replica of New York City.</p>
 									</li>
 									<li><h4>Share:</h4>
 									<p>As you begin your new project, publish the work you create to your friends or the public for critique and inspiration. </p>
@@ -23,6 +25,7 @@
 									<p>As you begin the construction of your own portfolio, connect with other like-minded folk from around the world and create your own personal or professional network.</p>
 									</li>
 								</ul>
+								</span>
 								</div><!--End Admin Area Div-->
 			</div> <!--End col2-->
 			<?php include "php/col3_footer.php"; ?>
