191141e3cd58a3e0611ee73ec8200b890fc1af94
Stephen Quenzer <swquenzer@gmail.com>
2013-10-29 11:11:01 -0400

    Big Update: Header Change
    -Changed to fixed header
    -Style changes
    -Header h1 font family and size change

--- include/nav.php
+++ include/nav.php
@@ -1,6 +1,6 @@
 <body>
-<div id="wrapper">
-	<div id="header">
+
+	<div id="header"><span id="headWrapper">
 		<div id="navBar">
 			<ul id="navList">
 				<li><a href="/wip/index.php">Home</a></li>
@@ -10,5 +10,6 @@
 				<li id="loginLink"><a id="loginLinkA" href="/wip/login.php">Default</a></li>
 			</ul>
 		</div> <!--End Nav Bar-->
-		<h1>A Work in Progress</h1>
-	</div> <!--End Header-->
+		<h1>~:{ A Work in Progress }:~</h1>
+	</span></div> <!--End Header-->
+	<div id="wrapper">
