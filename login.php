af8cd284193c3ce37d49e52fe5f6279c567a1dee
Stephen Quenzer <swquenzer@gmail.com>
2013-10-31 09:50:35 -0400

    Initial seperation of login form and form handler

--- login.php
+++ login.php
@@ -4,80 +4,11 @@
 </head>
 <?php include 'include/nav.php'; ?>
 		<div id="pageContent">
-			
+			//<? require ('forms/loginForm.php'); ?>
 			<div id="colMain"> <!-- ### MAIN CONTENT ### -->
 				<span id="contentHeader"><h1>Login</h1></span>
-				<?php
-						$continue = true;
-						include "include/db_connect.php";
-						if (!mysqli_select_db($dbHandle, "wip")) {
-							//throw new Exception("Could not connect to database");
-						}
-						//Check for cookie
-						if(isset($_COOKIE['ID_my_site']) & $continue == true) {
-							$username = $_COOKIE['ID_my_site'];
-							$pass = $_COOKIE['Key_my_site'];
-							$check = mysqli_query($dbHandle,   "SELECT * FROM members
-																WHERE email = '$username'")
-									or errors("queryUnsuccessful");
-							while($info = mysqli_fetch_array($check)) {
-								if($pass != $info['password']) { 
-								} else {
-									###TAKE OFF WIP
-									header("Location: members/".$username."/workstation.php");
-								}
-							}
-						}
-						if(isset($_POST['submit']) & $continue == true) {
-							if(!$_POST['email'] | !$_POST['pass'] & $continue == true) {
-								errors("emptyForm",$continue);
-							}
-							### Check against database
-							/*if magic quotes is enabled
-							if (!get_magic_quotes_gpc() & $continue == true) {
-								$_POST['email'] = addslashes($_POST['email']);
-							
-							*/
-							$query = "SELECT * 
-									  FROM members
-									  WHERE email = '".$_POST['email']."'";
-							$check = mysqli_query($dbHandle,$query); //Error handling
-							//if doesn't exist
-							$check2 = mysqli_num_rows($check);
-							if($check2 == 0 & $continue == true) {
-								errors("logEmailNull",$continue);
-							}
-							while($continue & $info = mysqli_fetch_array($check)) {
-								$_POST['pass'] = stripslashes($_POST['pass']);
-								$info['password'] = stripslashes($info['password']);
-								$_POST['pass'] = md5($_POST['pass']);
-								//if password is wrongs
-								if($_POST['pass'] != $info['password'] & $continue == true) {
-									errors("wrongPass",$continue);
-								} elseif ($continue == true) {
-									//Get username from user with correct email
-									$query = "SELECT username FROM members WHERE email='".$_POST['email']."'";
-									$getUsernameResult = mysqli_query($dbHandle,$query);
-									$usernameArray = mysqli_fetch_array($getUsernameResult);
-									//If login ok, add cookie
-									$hour = time() + 3600;
-									setcookie('ID_my_site', $usernameArray[0], $hour);
-									setcookie('Key_my_site', $_POST['pass'],$hour);
-									//redirect to members area
-									header("Location: ".$clientRootDir."members/".$usernameArray[0]."/workstation.php");
-								}
-							} //end while
-						} //end if
-						else { 
-					/*
-					 ### CATCH ERRORS ###
-					 catch(Exception $e) {
-						echo "Error: " . $e->getMessage();
-						echo '<br><a href="'.$_SERVER["PHP_SELF"].'">Try again?</a>';
-					}
-					*/
-						?>
-						<form id="globalForm" name="login" action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
+				
+						<form id="globalForm" name="login" action="forms/loginForm.php" method="post">
 							<fieldset>
 								<div id="globalFormContainer">
 									<p>
@@ -92,8 +23,8 @@
 								</div>
 							</fieldset>
 						</form>
-						<?php 
-						} //end else
+					<?php 
+						//} //end else
 					?> 
 					 <h1>Not a member yet?</h1>
 					 <p>
--- php/functions.php
+++ php/functions.php
@@ -14,24 +14,30 @@ function errors($errorType,&$continue) {
 			switch ($errorType) {
 				case "emptyForm":
 					echo 'You didn\'t complete all of the required fields';
+					dLog('You didn\'t complete all of the required fields');
 					break;
 				case "queryUnsuccessful":
 					echo 'Query unsuccessful';
+					dLog('Query unsuccessful');
 					break;
 				case "regUsernameInUse":
 					echo 'The email ' . $_POST['username'] . ' is already in use';
+					dLog('The email ' . $_POST['username'] . ' is already in use');
 					break;
 				case "wrongPass":
 					echo 'Your passwords don\'t match';
+					dLog('Your passwords don\'t match');
 					break;
 				case "dbConnectError":
 					echo 'The email ' . $_POST['username'] . ' is already in use';
+					dLog('The email ' . $_POST['username'] . ' is already in use');
 					break;
 				case "logEmailNull":
 					echo 'No account exists with that email';
+					dLog('No account exists with that email');
 					break;
 				default:
-					echo 'An error occured.';
+					dLog('An error occured.');
 					break;
 			}
 			echo '<br><a href="'.$_SERVER["PHP_SELF"].'">Try again?</a>';
@@ -45,4 +51,23 @@ function dLog($message) {
 	$contents.= "[".date("g:i a")."]:" .$message . "\r\n";
 	file_put_contents($log, $contents);
 }
+//If user is logged in (a cookie is set), return true
+function cookieCheck($dbHandle) { //Do I need to selec db again?
+	if(isset($_COOKIE['ID_my_site'])) {
+		$username = $_COOKIE['ID_my_site'];
+		$pass = $_COOKIE['Key_my_site'];
+		$check = mysqli_query($dbHandle,   "SELECT * FROM members
+											WHERE email = '$username'")
+				or errors("queryUnsuccessful"); //Proper error handling
+		while($info = mysqli_fetch_array($check)) {
+			if($pass != $info['password']) { 
+				return false;
+			} else {
+				return true;
+			}
+		}
+	}
+	return false;
+}
 ?>
+
